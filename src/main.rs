2c13fd8926fae887e39511cdabe47441082b0743
Hamidreza <hamid.khoshakhlagh@partisia.com>
2025-11-01 22:27:07 +0100

    impl. sort_by_addr_time

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@ use offline_memory_check::mem_op::MemOp;
 
 fn main(){
     let mut tr = Transcript::<u32>::new();
-    tr.push(MemOp::new(1u32, 0u32, 1u32, 42u32));
-    println!("{:?}", tr);
+    tr.push(MemOp::new(5u32, 1u32, 1u32, 9u32));
+    tr.push(MemOp::new(2u32, 2u32, 1u32, 7u32));
+    tr.push(MemOp::new(5u32, 0u32, 0u32, 0u32));
+    let sorted_tr = tr.sort_by_addr_time();
+    println!("{:?}", sorted_tr);
 }
--- src/transcript.rs
+++ src/transcript.rs
@@ -15,3 +15,16 @@ impl<F> Transcript<F> {
     }
 }
 
+impl<F: Clone + PartialOrd> Transcript<F> {
+    pub fn sort_by_addr_time(&self) -> Self {
+        let mut sorted_ops = self.ops.clone();
+        sorted_ops.sort_by(|a, b| {
+            a.addr
+                .partial_cmp(&b.addr)
+                .unwrap()
+                .then(a.time.partial_cmp(&b.time).unwrap())
+        });
+        Self { ops: sorted_ops}
+    }
+}
+
